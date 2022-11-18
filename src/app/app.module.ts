828f229041b89dd2fbf206d4301240a8e415e0cf
Sudhirr9 <117488466+Sudhirr9@users.noreply.github.com>
2022-11-18 13:44:53 -0500

    Added new Product alerts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
-  declarations: [AppComponent, HelloComponent, ProductListComponent],
+  declarations: [AppComponent, HelloComponent, ProductListComponent, ProductAlertsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
